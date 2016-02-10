399d935db3ee40765d93cc931435001ff74126bf
Pierre Baillet <pierre@baillet.name>
2016-02-10 17:14:34 +0100

    More stuff

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::error::Error;
 
 use staticfile::Static;
 
@@ -9,32 +10,52 @@ use router::{Router};
 use api;
 
 use data_format::FileMeta;
+use rustc_serialize::json;
 
-fn handler(req: &mut Request) -> IronResult<Response> {
-  let ref query = req.extensions.get::<Router>().unwrap().find("method").unwrap_or("/");
-  println!("{}", query);
-  Ok(Response::with(status::Ok))
+#[derive(RustcEncodable)]
+struct LogFileList {
+  files: Vec<String>
 }
 
+#[derive(RustcEncodable)]
+struct ErrorMessage {
+  message: String
+}
+
+fn handler(req: &mut Request) -> IronResult<Response> {
+  let query = req.extensions.get::<Router>().unwrap().find("method").unwrap_or("/");
+  if query == "list" {
+    let payload = match api::find_files("./logs") {
+      Ok(logs) => json::encode(&LogFileList{ files: logs.iter().map(|&ref file| file[1..].to_string()).collect() }).unwrap(),
+      Err(e) => json::encode(&ErrorMessage{message: e.description().to_string()}).unwrap()
+    };
+
+    Ok(Response::with((status::Ok, payload)))
+  } else {
+    println!("{}", query);
+    Ok(Response::with(status::Ok))
+  }
+}
 
 pub fn startup() {
+  println!("Starting HTTP Daemon...");
+
   match api::find_files("./logs") {
     Ok(logs) => println!("Files: {:?}", logs),
     Err(e) => println!("Error: {:?}", e)
   }
 
-  FileMeta::fast_meta("./logs/2015/03/24/ls-17:30:30.ajson".to_string());
+  // FileMeta::fast_meta("./logs/2015/03/24/ls-17:30:30.ajson".to_string());
 
   let mut mount = Mount::new();
 
   mount.mount("/logs", Static::new(Path::new("logs")));
+  mount.mount("/viewer", Static::new(Path::new("viewer")));
 
   let mut router = Router::new();
   router.get("/:method", handler);
   mount.mount("/api/1/", router);
 
+  println!("Open http://localhost:5001/viewer/");
   Iron::new(mount).http("0.0.0.0:5001").unwrap();
 }
-
-
-pub struct Daemon;
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,59 @@ extern crate iron;
 extern crate staticfile;
 extern crate mount;
 extern crate router;
+extern crate getopts;
 
 mod data_format;
 mod runner;
 mod daemon;
 mod api;
 
+use std::env;
+use getopts::Options;
+
 extern crate time;
+
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [-d] [-s] [COMMAND WITH PARAMETERS]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+
 fn main() {
-  daemon::startup();
 
-  let r = runner::Runner;
+  let args: Vec<String> = env::args().collect();
+  let program = args[0].clone();
 
-  let mut args = std::env::args();
-  let _ = args.next().unwrap();
-  let cmd = args.next().unwrap();
+  let mut opts = Options::new();
+  opts.optflag("s", "server", "start web server");
+  opts.optflag("d", "daemonize", "Daemonize the webserver, implies -s");
+  opts.optflag("h", "help", "print this help menu");
 
-  r.run(cmd.as_ref(), args.collect());
+  let matches = match opts.parse(&args[1..]) {
+    Ok(m) => { m }
+    Err(f) => { panic!(f.to_string()) }
+  };
+  if matches.opt_present("h") {
+    print_usage(&program, opts);
+    return;
+  }
+  if matches.opt_present("d") || matches.opt_present("s") {
+    daemon::startup();
+  } else {
+    if matches.free.len() > 0 {
+      let r = runner::Runner;
+
+      let mut cmd = matches.free.clone();
+      let params = cmd.split_off(1);
+
+      r.run(cmd[0].as_str(), params);
+    } else {
+      print_usage(&program, opts);
+      return;
+    }
+  };
+
+
+
 }
